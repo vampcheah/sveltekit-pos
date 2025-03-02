bcc8a123112a770a9edbfc3e59c3c2043d06696b
francischeah <vampcheah@gmail.com>
2025-03-03 00:17:20 +0800

    Update project name to reflect SvelteKit framework

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export const project = {
-	name: 'Svelte POS',
+	name: 'Sveltekit POS',
 	version: '0.0.1',
 	author: 'SveltePOS',
 	description: 'A simple POS system built with SvelteKit, TailwindCSS and Shadcn-Svelte'
