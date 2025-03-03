00efb39eee08afd2f01aeb6b66acabfa4b7a46c2
francischeah <vampcheah@gmail.com>
2025-03-03 18:44:21 +0800

    Update project author and version in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 export const project = {
 	name: 'Sveltekit POS',
-	version: '0.0.1',
-	author: 'SveltePOS',
+	version: '0.0.2',
+	author: 'FrancisXiaobu',
 	description: 'A simple POS system built with SvelteKit, TailwindCSS and Shadcn-Svelte'
 };
