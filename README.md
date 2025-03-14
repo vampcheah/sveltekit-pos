9e8d5c9529821c30c9ea9bca52b1a14901ca49fb
francischeah <vampcheah@gmail.com>
2025-03-14 11:30:41 +0800

    Bump project version to 0.0.7 and update README with new version details

--- README.md
+++ README.md
@@ -119,7 +119,7 @@ This project is licensed under the MIT License - see the LICENSE file for detail
 
 ## Version
 
-- current-dev
+- 0.0.7
   - Implementing currency to display price
   - Tax supported
   - Discount supported
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 export const project = {
 	name: 'Sveltekit POS',
 	name_short: 'SvPOS',
-	version: '0.0.6',
+	version: '0.0.7',
 	author: 'FrancisXiaobu',
 	description: 'A simple POS system built with SvelteKit, TailwindCSS and Shadcn-Svelte',
 	currency: 'MYR'
