abc7e3085f7ff2a2287cafe1135ede27cbd17520
francischeah <vampcheah@gmail.com>
2025-03-15 12:32:37 +0800

    Refactor layout in +page.svelte for improved responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <div class="flex h-screen flex-col items-center justify-center gap-4">
 	<h1>Welcome to Svelte-POS</h1>
 	<div class="flex w-full max-w-2xl flex-col gap-4 p-2">
-		<div class="flex gap-4">
+		<div class="flex flex-col gap-4 sm:flex-row">
 			<Button
 				class="basis-1/2 bg-blue-700 text-white hover:bg-blue-800"
 				variant="outline"
@@ -26,7 +26,7 @@
 				Go to POS Display
 			</Button>
 		</div>
-		<div class="flex gap-4">
+		<div class="flex flex-col gap-4 sm:flex-row">
 			<Button
 				class="basis-1/2 bg-blue-700 text-white hover:bg-blue-800"
 				variant="outline"
